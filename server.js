106dbd6b355ae2b3e34ea0d1b388649eaadc20a8
jacksonbaca07-arch <jacksonbaca07@gmail.com>
2026-08-21 23:15:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.end("Servidor funcionando correctamente 🚀");
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Roki Servidor funcionando correctamente");
 });
 
 server.listen(PORT, "0.0.0.0", () => {
