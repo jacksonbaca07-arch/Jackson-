19f1cb63c1f24cfd1344d65e081154008a635dd6
jacksonbaca07-arch <jacksonbaca07@gmail.com>
2026-08-21 23:41:11 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const http = require("http");
-
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Roki Servidor funcionando correctamente");
-});
-
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor funcionando en el puerto ${PORT}`);
-});
+{
+  "name": "jackson-server",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^5.1.0"
+  }
+}
